5145342c5f56db7045edd610c7367eea1b6e94c5
yunglleung1 <yungl.leung1@gmail.com>
2018-08-06 10:58:44 -0400

    Done.

--- app.rb
+++ app.rb
@@ -4,5 +4,19 @@ class App < Sinatra::Base
 		erb :index
 	end
 
+	get '/hello' do
+		status 200
+		erb :hello
+	end
+
+	get '/goodbye' do
+		status 200
+		erb :goodbye
+	end
+
+	get '/date' do
+		status 200
+		erb :date
+	end
 
 end
